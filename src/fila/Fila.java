3f0b635236c808461c794c6cb445cde714d9e559
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2023-08-17 09:37:32 -0300

    Estrutura fila e pilha criada

---
+++ src/fila/Fila.java
@@ -0,0 +1,44 @@
+package fila;
+import listaLigada.ListaLigada;
+
+public class Fila {
+
+	private ListaLigada fila = new ListaLigada();
+	
+	public void adicionar(Object novoElemento) {
+		fila.adicionaNoFinal(novoElemento);
+	}
+	
+	public void remover () {
+		fila.removeDoComeco();
+	}
+	
+	public boolean ehVazia() {
+		return fila.ehVazia();
+	}
+	
+	public Object poll() {
+		if (ehVazia()) {
+			return null;
+		}else {
+			Object primero_fila = fila.pegaPrimeiro();
+			fila.removeDoComeco();
+			return primero_fila;
+		}
+	}
+	
+	public boolean contem(Object elementoBuscado) {
+		return fila.contem(elementoBuscado);
+	}
+	
+	public int pegaTamanho() {
+		return fila.pegaTotalElementos();
+	}
+	
+}
+
+
+
+
+
+
